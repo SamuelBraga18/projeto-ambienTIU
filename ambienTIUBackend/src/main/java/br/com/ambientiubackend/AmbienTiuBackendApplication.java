7a05beecf5c65b70b0ff3c71867fcd690d1b4c9d
thyemelo <thyenmello13@gmail.com>
2026-05-10 21:33:56 -0300

    Spring Boot inicializado

---
+++ ambienTIUBackend/src/main/java/br/com/ambientiubackend/AmbienTiuBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ambientiubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbienTiuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmbienTiuBackendApplication.class, args);
+    }
+
+}
---
+++ ambienTIUBackend/src/test/java/br/com/ambientiubackend/AmbienTiuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ambientiubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbienTiuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
